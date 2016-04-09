a83f43bd4e395e1bb16cca3b040155df75de58d9
delans1 <Y8HZ1y$CWZPnJ$4s>
2016-04-10 01:47:44 +0200

    Add select entire table

--- src/test/scala/connect/CassandraSelectSpec.scala
+++ src/test/scala/connect/CassandraSelectSpec.scala
@@ -8,7 +8,7 @@ import com.datastax.driver.core.querybuilder.QueryBuilder._
 class CassandraSelectSpec extends FunSpec with Matchers {
 
   describe("A select statement on cassandra") {
-    it("should get information from emp") {
+    it("should get emp_first that is fred where id is 1") {
       val uri = CassandraConnectionUri("cassandra://localhost:9042/dev")
       val session = Helper.createSessionAndInitKeyspace(uri)
 
@@ -19,6 +19,17 @@ class CassandraSelectSpec extends FunSpec with Matchers {
       val result = session.execute(statement).one()
 
       result.getString("emp_first") should be ("fred")
+    }
+
+    it("should get list with rows containing all emp") {
+      val uri = CassandraConnectionUri("cassandra://localhost:9042/dev")
+      val session = Helper.createSessionAndInitKeyspace(uri)
+
+      val statement = select().all().from("emp")
+
+      val result = session.execute(statement).all()
+
+      result.size() should be > 0
     }
   }
 }
